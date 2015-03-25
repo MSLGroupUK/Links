86c4f8d015eb268801ee9f02ade44b9c3b495cd6
Thomas Digby <thomasdigby1@gmail.com>
2015-03-25 13:51:45 +0000

    Initial release

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # Links
+Extending the functionaity of the `target` attribute.
+
+##Usage
+
+```html
+<button data-target="pageTop">Back to top</button>
+<a href="#section-1" data-target="onPage">Scroll to Section 1</a>
+```
+
+```javascript
+Links.init();
+```
+
+Add a `data-target` attibute to the anchor and pass through a target as defined below:
+
+###pageTop
+Smoothscroll to the top of the page
+###onPage
+Smoothscroll to an element on the page
+###skip
+Jump to element and focus
+###newWindow
+Opens a new window, mostly used for social sharing so default size is 500 by 420
+
+##Release History
+
+v1.0.0 - Initial release
---
+++ links.js
@@ -0,0 +1,132 @@
+﻿var Links = {
+
+	selector: '[data-target]',
+	viewport: 'html, body',
+
+	targetNewWindow: 'new-window',
+	targetPageTop: 'page-top',
+	targetOnPage: 'on-page',
+	targetSkip: 'skip',
+
+	event: 'click',
+	eventNamespace: 'links',
+
+	offsetValue: 0,
+	scrollDuration: 600,
+
+	init: function () {
+		// detect if selector exists
+		if (!$(this.selector).length) return;
+
+		// run through selectors
+		this.loopSelectors();
+	},
+
+	loopSelectors: function () {
+
+		// for each selector
+		$(this.selector).each(function () {
+
+			// check bound events
+			var $this = $(this),
+				eventBound = Links.matchBoundEvent($this);
+
+			// if no event has been bound
+			if (eventBound) return;
+
+			// bind click event
+			Links.bindClick($this);
+		});
+	},
+
+	// event
+	bindClick: function ($this) {
+
+		// bind namespaced event
+		$this.on(this.event + '.' + this.eventNamespace, function (e) {
+
+			// get target event from data-target
+			var href = $this.attr('href'),
+				target = $this.data('target'),
+				targetEvent = Links.getTargetEvent(target);
+
+			// run target event
+			targetEvent(href);
+			e.preventDefault();
+		});
+	},
+
+	// targets
+	newWindow: function (href) {
+		// window parameters
+		var height = 420,
+			width = 500,
+			scrollBars = 'no',
+			resizable = 'yes';
+		// open new window with params
+		window.open(href, 'popup', 'width=' + width + ', height=' + height + ', scrollbars=' + scrollBars + ', resizable=' + resizable + '');
+	},
+	onPage: function (href) {
+		// scroll to offset of target
+		$(Links.viewport).stop().animate({
+			scrollTop: Math.ceil($(href).offset().top) - Links.offsetValue
+		}, Links.scrollDuration).promise().then(function () { });
+	},
+	pageTop: function () {
+		// scroll to top
+		$(Links.viewport).stop().animate({
+			scrollTop: 0
+		}, Links.scrollDuration);
+	},
+	skip: function (href) {
+		// focus accessibility links
+		$(href).attr('tabindex', '-1').focus();
+	},
+
+	// utils
+	matchBoundEvent: function ($this) {
+
+		// get click events
+		var linksBound = false;
+
+		// detect if any events are bound, !(!!) detects if item is not false, null or undefined, not neccessarily a check for a valid event object
+		var eventAll = this.getBoundEvents($this);
+		if (!(!!eventAll)) return false;
+
+		// get click event
+		var eventClick = eventAll[this.event];
+		if (!(!!eventClick)) return false;
+
+		// loop through bound click events
+		for (var i = 0; i < eventClick.length; i++) {
+			// check if event namespace exists
+			if (eventClick[i].namespace === this.eventNamespace) {
+				linksBound = true;
+			}
+		}
+
+		// return if click event has been bound
+		return linksBound;
+	},
+	getTargetEvent: function (target) {
+
+		// match target and return relevant function
+		switch (target) {
+			case this.targetNewWindow:
+				return this.newWindow;
+				break;
+			case this.targetOnPage:
+				return this.onPage;
+				break;
+			case this.targetSkip:
+				return this.skip;
+				break;
+			case this.targetPageTop:
+				return this.pageTop;
+		}
+	},
+	getBoundEvents: function ($this) {
+		// return events bound to $this
+		return $._data($this[0], 'events');
+	}
+};
